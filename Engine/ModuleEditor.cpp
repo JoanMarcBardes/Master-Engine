a91dd1fa3506d379ee94d4c65e55334f69f02724
JoanMarcBardes <71397864+JoanMarcBardes@users.noreply.github.com>
2020-11-28 11:43:23 +0100

    About

--- Engine/ModuleEditor.cpp
+++ Engine/ModuleEditor.cpp
@@ -428,9 +428,9 @@ update_status ModuleEditor::MainMenuBar()
 	{		
 		if (ImGui::MenuItem("Github repository"))
 		{
-			RequestBrowser("http://www.google.com");
+			RequestBrowser("https://github.com/JoanMarcBardes/Master-Engine");
 		}
-		ImGui::MenuItem("About Dear ImGui", NULL, &showAbout);
+		ImGui::MenuItem("About", NULL, &showAbout);
 
 		if (ImGui::BeginMenu("Windows"))
 		{
@@ -450,12 +450,43 @@ update_status ModuleEditor::MainMenuBar()
 
 void ModuleEditor::WindowAbout(bool* p_open)
 {
-	if (!ImGui::Begin("About Dear ImGui", p_open, ImGuiWindowFlags_AlwaysAutoResize))
+	if (!ImGui::Begin("About", p_open, ImGuiWindowFlags_AlwaysAutoResize))
 	{
 		ImGui::End();
 		return;
 	}
-	ImGui::Text("Options");
+
+	ImGui::Text(
+		"Joan-Marc-Engine\n"
+		"Joan Marc Engine it's an engine developed during the Master in Advanced Programming for AAA Video Games.\n"
+		"You can found the poryect on : https://github.com/JoanMarcBardes/Master-Engine \n\n"
+
+		"Authors\n"
+		"Joan Marc Bardés Vera, guithub account : https://github.com/JoanMarcBardes \n\n"
+
+		"License\n"
+		"This project is under the MIT License - see the[LICENSE.md] file for details\n\n"
+
+		"Guide how to use the engine\n\n"
+
+		"Camera controls:\n"
+		" * Q / E go up / down.\n"
+		" * W / S move forward and backward.\n"
+		" * A / D move left and right.\n"
+		" * Up / Down and Left / Right arrows for eotate the camera.\n"
+		" * Right mouse click movement.\n"
+		" * Alt + Right free look around.\n"
+		" * Mouse wheel for zoom inand out.\n"
+		" * Alt + Left click for orbit the object.\n"
+		" * F for focus the camera around the geometry.\n"
+		" * Holding SHIFT duplicates movement speed.\n"
+		" * O for enable / disable orbit the object with the movement.\n\n"
+
+		"Drag to window engine a.fbx file for replace the model.\n"
+		"Drag to window engine a.png / jpg / dds file for replace the texture.\n\n"
+
+		"In configuration window, can configureand visualise several options of the engine.\n"
+	);
 
 	ImGui::End();
 }
